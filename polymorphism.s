0b690ab6fe34186259fb023d430d9d50d07a6947
Razvan Ludosanu <r.ludosanu@gmail.com>
2018-03-16 13:40:00 +0000

    File cleaned

--- polymorphism.s
+++ polymorphism.s
@@ -1,13 +1,8 @@
-section	.data
-
 section	.text
-	global	_start
 	global	_timestamp
 	global	_prng
 	global	_urand
 	global	_byterpl
-	global	_fn
-	global	_stackdump
 
 ;; -----------------------------------------------------------------------------
 ;; Junk instructions
@@ -38,70 +33,7 @@ dd 0x90d63948	; cmp rsi, rdx
 ;; Static strings
 ;; -----------------------------------------------------------------------------
 _urandom:
-.string db '/dev/urandom', 0
-
-_hello:
-.string db 'Hello World !', 0
-
-;; -----------------------------------------------------------------------------
-;; SYNOPSIS
-;;		size_t	_fn(char *ptr)
-;;
-;; DESCRIPTION
-;;		Returns the length in bytes of the string pointed by ptr.
-;; ----------------------------------------------------------------------------
-_fn:
-.start:
-	enter	16, 0
-	nop
-	nop
-	nop
-	nop
-	xor		rcx, rcx
-	nop
-	nop
-	nop
-	nop
-	mov		qword [rsp], rdi
-	nop
-	nop
-	nop
-	nop
-	cmp		rdi, 0
-	je		_fn.return
-	jmp		_fn.loop
-.loop:
-	nop
-	nop
-	nop
-	nop
-	cmp		byte [rdi], 0
-	je		_fn.return
-	nop
-	nop
-	nop
-	nop
-	inc		rcx
-	nop
-	nop
-	nop
-	nop
-	inc		rdi
-	jmp		_fn.loop
-.return:
-	nop
-	nop
-	nop
-	nop
-	mov		rdi, qword [rsp]
-	nop
-	nop
-	nop
-	nop
-	mov		rax, rcx
-	leave
-	ret
-.end:
+	db '/dev/urandom', 0
 
 ;; -----------------------------------------------------------------------------
 ;; NAME
@@ -253,7 +185,6 @@ _urand_end:
 ;;		its input from /dev/urandom.
 ;; ----------------------------------------------------------------------------
 _byterpl:
-.start:
 	enter	32, 0
 	push	rdi
 	push	rsi
@@ -264,43 +195,43 @@ _byterpl:
 	mov		qword [rbp - 16], rax			; temporary offset
 	mov		qword [rbp - 24], rax			; backup value for PRNG
 
-.loop:
+_byterpl_loop:
 	cmp		qword [rbp - 8], rsi			; did we go too far ?
-	jge		_byterpl.end					; yeup, we're done
+	jge		_byterpl_end					; yeup, we're done
 	
 	cmp		byte [rdi], 0x90				; if we found a 0x90
-	je		_byterpl.init					; check if we have 5 in a row
+	je		_byterpl_init					; check if we have 5 in a row
 
 	inc		rdi								; move to the next byte
 	inc		qword [rbp - 8]					; increment global offset
 	
-	jmp		_byterpl.loop					; check next byte
+	jmp		_byterpl_loop					; check next byte
 
-.init:
+_byterpl_init:
 	xor		rcx, rcx						; clear counter
 	mov		r10, rdi						; set up temporary pointer
 	mov		rax, qword [rbp - 8]
 	mov		qword [rbp - 16], rax
 
-.check:
+_byterpl_check:
 	cmp		qword [rbp - 16], rsi			; did we go too far ?
-	jge		_byterpl.end					; yeup, we're done
+	jge		_byterpl_end					; yeup, we're done
 
 	cmp		byte [r10], 0x90				; is the byte different from 0x90 ?
-	jne		_byterpl.test					; yeup, check the counter
+	jne		_byterpl_test					; yeup, check the counter
 
 	inc		rcx								; increase counter
 	inc		r10								; move to next byte
 	inc		qword [rbp - 16]				; increase temporary counter
-	jmp		_byterpl.check					; loop back
+	jmp		_byterpl_check					; loop back
 
-.test:
+byterpl_test:
 	cmp		rcx, 4							; is our counter up to 4 ?
-	jge		_byterpl.replace				; great, replace at this offset
+	jge		_byterpl_replace				; great, replace at this offset
 	inc		rdi								; move to next byte
-	jmp		_byterpl.loop					; nope, back to main loop...
+	jmp		_byterpl_loop					; nope, back to main loop...
 
-.replace:
+_byterpl_replace:
 	push	rdi								; save up
 	push	rsi								; save up
 	push	rdx								; save up
@@ -310,15 +241,16 @@ _byterpl:
 	mov		rsi, BYTERPL_MAX				; maximum byte array index
 	mov		rdx, qword [rbp - 24]			; index def = current
 	call	_urand							; call urand
+	
 	mov		qword [rbp - 24], rax			; store result
 	cmp		rax, -1							; if _urand returned -1, we need to get the fuck out
-	je		_byterpl.end					; get the fuck out then ...
+	je		_byterpl_end					; get the fuck out then ...
 	
 	pop		rdx								; restore
 	pop		rsi								; restore
 	pop		rdi								; restore
 
-.insert:
+_byterpl_insert:
 	mov		rax, qword [rbp - 24]			; get the replacing bytes index
 	lea		r11, [rel _bytes + rax * 4]		; get the value at this index
 	mov		r11d, dword [r11]				; save it up
@@ -328,85 +260,12 @@ _byterpl:
 	add		rdi, 5							; move main pointer +5
 	add		qword [rbp - 8], 5				; move global offset +5
 	
-	jmp		_byterpl.loop					; jump back to main loop...
+	jmp		_byterpl_loop					; jump back to main loop...
 
-.end:
+_byterpl_end:
 	mov		rax, qword [rbp - 24]
 	pop		r10
 	pop		rsi
 	pop		rdi
 	leave
 	ret
-
-_start:
-	enter	16, 0
-
-;; -----------------------------------------------------------------------------
-;; Copy the function _fn on stack
-;; -----------------------------------------------------------------------------
-_stackdump:
-	;; Get code length
-	lea		rdi, [rel _fn.start]
-	lea		rsi, [rel _fn.end]
-	sub		rsi, rdi
-	mov		qword [rsp], rsi
-	xor		rcx, rcx
-	mov		rcx, rsi
-
-	;; Setup destination pointer
-	mov		rdi, rsp
-	sub		rdi, rcx
-
-	;; Setup source pointer
-	lea		r10, [rel _fn.start]
-
-.copy:
-	;; If last byte break
-	cmp		rcx, 0
-	je		_replacebytes
-
-	;; Copy byte from opcaodes to stack
-	mov		al, byte [r10]
-	mov		byte [rdi], al
-	
-	;; Move up pointers
-	inc		r10
-	inc		rdi
-	
-	;; Decrement counter
-	dec		rcx
-	
-	;; Loop back
-	jmp		_stackdump.copy
-.end:
-
-;; -----------------------------------------------------------------------------
-;; Replace nopsleds on stack with junk instructions
-;; -----------------------------------------------------------------------------
-_replacebytes:
-	;; Replace NOPs on stack
-	mov		rsi, qword [rsp]
-	mov		rdi, rsp
-	sub		rdi, rsi
-	mov		r10, qword [rsp]
-	sub		rsp, r10
-	call	_byterpl
-	mov		rdi, rax
-	add		rsp, r10
-
-;; -----------------------------------------------------------------------------
-;; Call function on stack
-;; -----------------------------------------------------------------------------
-_stackcall:
-	;; Call function from stack
-	mov		r10, qword [rsp]
-	sub		rsp, r10
-	lea		rdi, [rel _hello.string]
-	call	rsp
-	mov		rdi, rax
-	add		rsp, r10
-
-_end:
-	leave
-	mov		rax, 60
-	syscall
